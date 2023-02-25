14b86c12d6e6f59e25c19618989fc2c6c0397615
Micheal O'Donovan <mikeodonovan3@gmail.com>
2023-02-25 15:32:18 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
+import { FacebookShareButton, FacebookIcon } from 'react-share';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <h1>Social Media Links</h1>
+
+    <a href="https://www.facebook.com/dialog/share?app_id=1456312395501895&display=popup&href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2F&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer">test straight url link</a>
+
+    <h1>Package also doesn't pass the quote / message!</h1>
+    <div>
+      <FacebookShareButton
+        url={'https://www.google.com'}
+        quote={'dogs dogs dogs'}
+        hashtag="#muo"
+      >
+        <FacebookIcon size={32} round />
+      </FacebookShareButton>
+    </div>
+
     </div>
   );
 }
