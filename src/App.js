18770eb6c15383242308f1df25474b239405d6a3
Micheal O'Donovan <mikeodonovan3@gmail.com>
2023-02-28 11:34:12 +0000

    update

--- src/App.js
+++ src/App.js
@@ -8,14 +8,20 @@ function App() {
 
     <h1>Social Media Links</h1>
 
-    <a href="https://www.facebook.com/dialog/share?app_id=1456312395501895&display=popup&href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2F&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer">test straight url link</a>
+    <div>
+    <a style={{marginTop: "50px"}} href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Famazon.co.uk&quote=dogs">old straight link url</a>
+    </div>
 
-    <h1>Package also doesn't pass the quote / message!</h1>
+    <div style={{marginTop: "50px"}}>
+    <a href="https://www.facebook.com/dialog/share?app_id=145634995501895&display=popup&href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2F&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer">new straight link url</a>
+    </div>
+
+    <h1>Package also doesn't pass the quote / message:</h1>
     <div>
       <FacebookShareButton
         url={'https://www.google.com'}
-        quote={'dogs dogs dogs'}
-        hashtag="#muo"
+        quote={'dogs, dogs, dogs I love dogs!'}
+        hashtag="#woof"
       >
         <FacebookIcon size={32} round />
       </FacebookShareButton>
