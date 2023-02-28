0d94634b4005ca10f54ae36ab743f2985cd39b78
Micheal O'Donovan <mikeodonovan3@gmail.com>
2023-02-28 15:02:04 +0000

    facebook issue

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import { FacebookShareButton, FacebookIcon } from 'react-share';
 import './App.css';
+import { Hello } from './Test.js';
 
 function App() {
+
+
+  Hello();
+
   return (
     <div className="App">
 
-    <h1>Social Media Links</h1>
-
     <div>
-    <a style={{marginTop: "50px"}} href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Famazon.co.uk&quote=dogs">old straight link url</a>
+      <a style={{marginTop: "50px"}} href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Famazon.co.uk&quote=dogs">old straight link url</a>
     </div>
 
     <div style={{marginTop: "50px"}}>
-    <a href="https://www.facebook.com/dialog/share?app_id=145634995501895&display=popup&href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2F&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer">new straight link url</a>
+      <a href="https://www.facebook.com/dialog/share?app_id=145634995501895&display=popup&href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2F&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer">new straight link url</a>
     </div>
 
     <h1>Package also doesn't pass the quote / message:</h1>
