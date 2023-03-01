8f08772a4d37fbedef147d172f593c857bca07fd
Micheal O'Donovan <mikeodonovan3@gmail.com>
2023-03-01 14:12:22 +0000

    update

--- src/App.js
+++ src/App.js
@@ -11,24 +11,27 @@ function App() {
   return (
     <div className="App">
 
-    <div>
-      <a style={{marginTop: "50px"}} href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Famazon.co.uk&quote=dogs">old straight link url</a>
+    <div style={{marginTop: "50px"}}>
+      <a target="blank" href="https://www.facebook.com/sharer/sharer.php?u=https://www.comicrelief.com/rednoseday&quote=Rednoseday%202023!">old straight link url</a>
     </div>
 
     <div style={{marginTop: "50px"}}>
-      <a href="https://www.facebook.com/dialog/share?app_id=145634995501895&display=popup&href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2F&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer">new straight link url</a>
+      <a target="blank" href="https://www.facebook.com/dialog/share?app_id=145634995501895&display=popup&href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2F&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer">new straight link url</a>
     </div>
 
-    <h1>Package also doesn't pass the quote / message:</h1>
-    <div>
-      <FacebookShareButton
-        url={'https://www.google.com'}
-        quote={'dogs, dogs, dogs I love dogs!'}
-        hashtag="#woof"
-      >
-        <FacebookIcon size={32} round />
-      </FacebookShareButton>
-    </div>
+    <div style={{marginTop: "50px"}}>
+      <h1>Package also doesn't pass the quote / message:</h1>
+      <div>
+        <FacebookShareButton
+          url={'https://www.google.com'}
+          quote={'dogs, dogs, dogs I love dogs!'}
+          hashtag="#woof"
+        >
+          <FacebookIcon size={32} round />
+        </FacebookShareButton>
+      </div>
+    </div>
+
 
     </div>
   );
